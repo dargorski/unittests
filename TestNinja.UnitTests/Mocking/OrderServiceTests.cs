986d3964bf26e4ef49eed43eec8cb8b6b2f017ee
Dariusz Górski <dariuszgoorski@gmail.com>
2018-05-15 10:17:51 +0200

    czyszczenie

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_ShouldStoreOrder()
+        {
+            var order = new Order();
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));         
+        }
+    }
+}
--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,24 @@ using TestNinja.Mocking;
 namespace TestNinja.UnitTests.Mocking
 {
     [TestFixture]
-    class VideoServiceTests
+    public class VideoServiceTests
     {
+        private Mock<IFileReader> _fileReader;
+        private VideoService _videoService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new FakeFileReader());            
+            _fileReader.Setup(fr => fr.Read(It.IsAny<string>())).Returns("");
 
-            var result = service.ReadVideoTitle();
+            var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
